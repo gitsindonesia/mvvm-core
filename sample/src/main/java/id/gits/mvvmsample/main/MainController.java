b00fc9a05d4f9dac717cfdf9ff6d1aaa9ba0b7e1
Ibnu Sina W <punyaibun@gmail.com>
2016-04-05 07:31:12 +0700

    fix: remove unused commented code

--- sample/src/main/java/id/gits/mvvmsample/main/MainController.java
+++ sample/src/main/java/id/gits/mvvmsample/main/MainController.java
@@ -1,7 +1,6 @@
 package id.gits.mvvmsample.main;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import id.gits.mvvmcore.controller.GitsController;
 import id.gits.mvvmsample.databinding.MainActivityBinding;
@@ -10,10 +9,6 @@ import id.gits.mvvmsample.databinding.MainActivityBinding;
  * Created by ibun on 30/03/16.
  */
 public class MainController extends GitsController<MainActivityVM, MainActivityBinding> {
-    /*@Override
-    public MainActivityVM createViewModel(AppCompatActivity activity, MainActivityBinding binding) {
-        return new MainActivityVM(activity, this, binding);
-    }*/
 
     @Override
     public MainActivityVM getmViewModel(MainActivityBinding binding) {
